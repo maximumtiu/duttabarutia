5b5d3358d05852891ff6c471998b9a104bd2c189
Megan Tiu <megantiu@me.com>
2016-08-31 20:30:27 -0400

    Renaming tiger -> tigers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160901000537) do
+ActiveRecord::Schema.define(version: 20160901002724) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tigers", force: :cascade do |t|
     t.string  "location"
